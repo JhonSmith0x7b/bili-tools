be7cdcc49cae0d0468eea42f41c6bbb19511fba7
JhonSmith0x7b <JhonSmith0x7b@github.com>
2023-12-26 17:15:40 +0800

    add two tool scripts

---
+++ get-bullets.py
@@ -0,0 +1,108 @@
+import requests
+import json
+import sounddevice as sd
+import numpy
+import json
+import time
+import traceback
+import os
+import io
+import scipy.io.wavfile
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def get_bullets(room_id:str) -> list[tuple[str, str]]:
+    url = f"https://api.live.bilibili.com/xlive/web-room/v1/dM/gethistory?roomid={room_id}&room_type=0"
+    # api return ten bullets
+    headers = {
+        "authority": "api.live.bilibili.com",
+        "accept": "application/json",
+        "accept-language": "zh-CN",
+        "origin": "https://live.bilibili.com",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+    }
+    resp = requests.get(url, headers=headers)
+    data = json.loads(resp.text)
+    re = []
+    room = data['data']['room']
+    for line in room:
+        re.append((line['nickname'], line['text']))
+    return re
+
+
+def tts(text: str) -> None:
+    url = os.environ.get("TTS_ENDPOINT")
+    resp = requests.get(url, params={
+        "text": requests.utils.quote(text),
+        "sdp_ratio": 0.2,
+        "noise_scale": 0.6,
+        "noise_scale_w": 0.8,
+        "length_scale": 1.0
+    })
+    temp_audio = io.BytesIO(resp.content)
+    rate, data = scipy.io.wavfile.read(temp_audio)
+    sd.play(data, rate, blocking=True)
+
+
+def gpt(text: str) -> str:
+    if not (text.startswith("::") or text.startswith("：：")): return None
+    endpoint = os.environ.get("AZURE_ENDPOINT")
+    api_key = os.environ.get("AZURE_API_KEY")
+    model = os.environ.get("AZURE_MODEL")
+    url = f"{endpoint}/openai/deployments/{model}/chat/completions?api-version=2023-03-15-preview"
+    resp = requests.post(
+        url=url,
+        headers={"Content-Type": "application/json", "api-key": api_key},
+        json={
+            "messages": [
+                {
+                    "role": "system",
+                    "content": "你是个中文助手, 同时是个万能的女仆, 说话要像木之本櫻一样可爱, 需要保证你的回复少于30字. "
+                },
+                {
+                    "role": "user",
+                    "content": text
+                }
+            ]
+        }
+    )
+    if resp.json()['choices'][0]['finish_reason'] == "content_filter":
+        return "呼呼呼!!!小鸣决定不回答这个问题!"
+    return resp.json()['choices'][0]['message']['content']
+
+
+def loop_main() -> None:
+    simple_bk = []
+    while True:
+        try:
+            re = get_bullets(os.environ.get("ROOM_ID"))
+            start = 0 if len(re)-5<0 else len(re)-5
+            for i in range(start, len(re)):
+                text = re[i][1]
+                if text in simple_bk: continue
+                try:
+                    tts(text)
+                    gpt_re = gpt(text)
+                    if gpt_re != None: tts(gpt_re[:30])
+                except Exception as e:
+                    print(e)
+                    traceback.print_exc()
+                simple_bk.append(text)
+            simple_bk = simple_bk[len(simple_bk)-100:]
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+        time.sleep(5)
+
+
+def test() -> None:
+    re = gpt("::测试弹幕")
+    tts(re)
+
+
+
+if __name__ == '__main__':
+    loop_main()
+    # test()
+
---
+++ simple_tts_api.py
@@ -0,0 +1,123 @@
+import sys
+sys.path.append("./bert_vits2")
+import os
+import torch
+from bert_vits2 import utils
+from bert_vits2.text.cleaner import clean_text
+from bert_vits2.text import cleaned_text_to_sequence, get_bert
+from bert_vits2 import commons
+from bert_vits2.infer import infer, latest_version, get_net_g, get_text, infer_multilang
+import numpy
+import flask
+import json
+import requests
+import io
+import datetime
+import scipy.io.wavfile
+
+
+device = (
+        "cuda:0"
+        if torch.cuda.is_available()
+        else (
+            "mps"
+            if sys.platform == "darwin" and torch.backends.mps.is_available()
+            else "cpu"
+        )
+    )
+if device == "mps":
+    # os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
+    device = "cpu"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+print(device)
+model_path = sys.argv[1]
+hps = utils.get_hparams_from_file(sys.argv[2])
+version = hps.version if hasattr(hps, "version") else latest_version
+net_g = get_net_g(
+    model_path=model_path, version=version, device=device, hps=hps
+)
+
+def simple_audio(
+    text: str,
+    language: str = 'ZH',
+    sdp_ratio: float = 0.2,
+    noise_scale: float = 0.6,
+    noise_scale_w: float = 0.8,
+    length_scale: float = 1.0
+) -> tuple[numpy.ndarray, int]:
+    bert, ja_bert, en_bert, phones, tones, lang_ids = get_text(text, language, hps, device)
+    x_tst = phones.to(device).unsqueeze(0)
+    tones = tones.to(device).unsqueeze(0)
+    lang_ids = lang_ids.to(device).unsqueeze(0)
+    bert = bert.to(device).unsqueeze(0)
+    ja_bert = ja_bert.to(device).unsqueeze(0)
+    en_bert = en_bert.to(device).unsqueeze(0)
+    x_tst_lengths = torch.LongTensor([phones.size(0)]).to(device)
+    del phones
+    sid = list(hps.data.spk2id.keys())[0]
+    speakers = torch.LongTensor([hps.data.spk2id[sid]]).to(device)
+    audio = infer(
+        text=text,
+        sdp_ratio=sdp_ratio,
+        noise_scale=noise_scale,
+        noise_scale_w=noise_scale_w,
+        length_scale=length_scale,
+        sid=sid,
+        language=language,
+        hps=hps,
+        net_g=net_g,
+        device=device,
+        reference_audio=None,
+        emotion=None,
+        skip_start=False,
+        skip_end=False
+    )
+    # audio = (
+    #     net_g.infer(
+    #         x_tst,
+    #         x_tst_lengths,
+    #         speakers,
+    #         tones,
+    #         lang_ids,
+    #         bert,
+    #         sdp_ratio=sdp_ratio,
+    #         noise_scale=noise_scale,
+    #         noise_scale_w=noise_scale_w,
+    #         length_scale=length_scale,
+    #     )[0][0, 0]
+    #     .data.cpu()
+    #     .float()
+    #     .numpy()
+    # )
+    del x_tst, tones, lang_ids, bert, x_tst_lengths, speakers, ja_bert, en_bert
+    return (audio, hps.data.sampling_rate)
+
+
+app = flask.Flask(__name__)
+
+
+@app.route("/simple-tts")
+def simple_tts() -> flask.Response:
+    text = flask.request.args.get("text")
+    try:
+        sdp_ratio = float(flask.request.args.get('sdp_ratio'))
+        noise_scale = float(flask.request.args.get('noise_scale'))
+        noise_scale_w = float(flask.request.args.get('noise_scale_w'))
+        length_scale = float(flask.request.args.get('length_scale'))
+    except:
+        sdp_ratio = 0.2
+        noise_scale = 0.6
+        noise_scale_w = 0.8
+        length_scale = 1.0
+    text = requests.utils.unquote(text)
+    audio, rate = simple_audio(text, sdp_ratio=sdp_ratio, noise_scale=noise_scale, noise_scale_w=noise_scale_w, length_scale=length_scale)
+    audio_buffer = io.BytesIO()
+    scipy.io.wavfile.write(audio_buffer, rate, audio)
+    return flask.send_file(
+        audio_buffer,
+        mimetype="audio/wav"
+    )
+
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", "12300")
