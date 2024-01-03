1a0116371a55207118eba7f85894a235625890b5
JhonSmith0x7b <JhonSmith0x7b@github.com>
2024-01-03 18:30:26 +0800

    add qt sample

--- qt_sample.py
+++ qt_sample.py
@@ -1,15 +1,27 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWebEngineWidgets import *
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
 
 
-class MainWindow(QMainWindow):
+class WebWindow(QWebEngineView):
     def __init__(self) -> None:
         super().__init__()
-        self.setWindowTitle("Hello World")
+        self.setWindowTitle("Live2D")
+        self.page().setBackgroundColor(Qt.GlobalColor.transparent)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setWindowFlags(
+            Qt.WindowType.WindowStaysOnTopHint |
+            Qt.WindowType.FramelessWindowHint 
+            # Qt.WindowType.WindowTransparentForInput
+        )
+        
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    view = WebWindow()
+    view.load(QUrl("http://192.168.1.7:12301/Samples/TypeScript/Demo/"))
+    view.resize(600, 400)
+    view.show()
     app.exec()
