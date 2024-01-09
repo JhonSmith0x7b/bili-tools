c479c799679299b307a40d9cd965629cd25bf49b
JhonSmith0x7b <JhonSmith0x7b@github.com>
2024-01-09 08:37:52 +0800

    fix

--- build_qt_bullets.ps1
+++ build_qt_bullets.ps1
@@ -2,5 +2,5 @@
 pyinstaller -w -y ./qt_bullets.py
 
 Copy-Item ./.env -Destination ./dist/qt_bullets -Force
-Copy-Item -Path ./prompts/ -Destination ./dist/qt_bullets/prompts -Force
-Copy-Item -Path ./resources/ -Destination ./dist/qt_bullets/resources/ -Force
+Copy-Item -Path ./prompts/ -Destination ./dist/qt_bullets/prompts -Recurse -Force
+Copy-Item -Path ./resources/ -Destination ./dist/qt_bullets/resources/ -Recurse -Force
