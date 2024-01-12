ad08dea5f53543b000270dd22b39412962dc90e5
JhonSmith0x7b <JhonSmith0x7b@github.com>
2024-01-12 15:49:02 +0800

    change import

--- qt_bullets.py
+++ qt_bullets.py
@@ -1,9 +1,8 @@
 import sys
 from PySide6.QtGui import QCloseEvent
-from PySide6.QtWebEngineWidgets import *
-from PySide6.QtWidgets import *
-from PySide6.QtCore import *
-from PySide6.QtGui import *
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit
+from PySide6.QtCore import Qt, QObject, Signal, QUrl, QSize, QThread
 import time
 import common
 import requests
